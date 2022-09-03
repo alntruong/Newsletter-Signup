35fa48a22bd6675d466d915c07a51156a2a77cf7
Alan Truong <alntruong@ucdavis.edu>
2022-09-02 17:07:54 -0700

    initial commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express")
-
 const bodyParser = require("body-parser")
+const request = require("request")
 
 const port = 3000
 
@@ -11,5 +11,5 @@ app.get("/", (req, res) => {
 })
 
 app.listen(port, () => {
- console.log("server is running on port 3000");
+ console.log("Server is running on port 3000");
 })
