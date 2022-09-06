130a5e97d4fbee01d244d1a0ea613b7d08dc32e2
Alan Truong <alntruong@ucdavis.edu>
2022-09-06 15:41:56 -0700

    notes

--- app.js
+++ app.js
@@ -6,15 +6,23 @@ const port = 3000
 
 const app = express()
 
+//our image and css did not appear because they are stored in a local location, making them a static page.
+//in order for our server to serve up static file(s), then we need to use a special function of express called static.
+//app.use(express.static("name_of_folder")) provides the path of our static file(s).
+//the path of the file(s) should be relative to the folder created (public).
 app.use(express.static("public"));
+
+//standard setting to enable body-parser
 app.use(bodyParser.urlencoded({extended: true}));
 
+//app.get is when the user request to GET the homepage, which in this case, sends back signup.html
 app.get("/", (req, res) => {
  res.sendFile(__dirname + "/signup.html")
 })
 
-//post is where the user submits data so that the server can go retrive the info
+//app.post is set up so that when the user requests to POST data, the server can go retrieve that data
 app.post("/", (req,res) => {
+ //the app utilizes body-parser to identify the location of the data
  var firstName = req.body.fName;
  var lastName = req.body.lName;
  var email = req.body.email;
@@ -24,3 +32,4 @@ app.post("/", (req,res) => {
 app.listen(port, () => {
  console.log("Server is running on port 3000");
 })
+
